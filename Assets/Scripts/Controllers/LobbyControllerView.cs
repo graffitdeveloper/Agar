f38a48d5061fe03a975756354ebf60b82b3b4539
Pavel Serebryakov <serebryakov@smartproject.ua>
2019-10-26 11:36:55 +0300

    some first panel and first controller. And first font and first button

--- Assets/Scripts/Controllers/LobbyControllerView.cs
+++ Assets/Scripts/Controllers/LobbyControllerView.cs
@@ -1,11 +1,28 @@
 using gRaFFit.Agar.Controllers.GameScene.MainControllers;
 using gRaFFit.Agar.Models.ControllerSwitcherSystem;
+using gRaFFit.Agar.Views.UIPanelSystem;
 
 namespace Controllers {
 	public class LobbyControllerView : AControllerView {
-		public override void Activate() {
-		}
 
 		public override ControllerType Type => ControllerType.Lobby;
+
+
+		public override void Activate() {
+			var mainPanel = UIManager.Instance.ShowPanel<MainPanelView>();
+			mainPanel.SignalOnPlayButtonClicked.AddListener(OnPlayButtonClicked);
+		}
+
+		public override void Deactivate() {
+			var mainPanel = UIManager.Instance.GetPanel<MainPanelView>();
+			mainPanel.SignalOnPlayButtonClicked.RemoveListener(OnPlayButtonClicked);
+			mainPanel.Hide();
+
+			base.Deactivate();
+		}
+
+		private void OnPlayButtonClicked() {
+			ControllerSwitcher.Instance.SwitchController(ControllerType.Game);
+		}
 	}
 }
